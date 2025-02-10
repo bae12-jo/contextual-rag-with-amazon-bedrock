bcb48e55a4f0991a09f6106ac23cb18d4e92aa48
bae12-jo <bae12.jo@gmail.com>
2025-02-11 03:23:54 +0900

    Add cdk and modify notebook

---
+++ contextual-retrieval/test/contextual-retrieval.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ContextualRetrieval from '../lib/contextual-retrieval-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/contextual-retrieval-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ContextualRetrieval.ContextualRetrievalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
